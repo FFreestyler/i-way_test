2b3c18025db0f393c9825ca9d6865f0499c6b2f6
Danil <freeestyler231@gmail.com>
2023-11-29 21:48:22 +0700

    Remove time.sleep function

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -28,27 +27,21 @@ def driver():
 
     driver.find_element(By.TAG_NAME, "input").send_keys(u"тестирование", Keys.RETURN)
 
-    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.products-list')))
-
     driver.execute_script("window.scrollTo(0, 250);")
-
-    time.sleep(5)
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='button action-button blue']")))
 
     check = 0
     for item in driver.find_elements(By.XPATH, "//div[@class='button action-button blue']"):
         if check == 3:
             break
-        item.click()
+        driver.execute_script("arguments[0].click();", item)
         check += 1
 
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//span[contains(text(), '3')]")))
-
     driver.find_element(By.XPATH, '//a[contains(@href,"/cart")]').click()
 
     test_price_difference(driver)
-
     driver.find_element(By.XPATH, "//button[@class='button cart-item__actions-button cart-item__actions-button--delete light-blue']").click()
-
     test_price_difference(driver)
 
     driver.close()
